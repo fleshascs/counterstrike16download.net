baa026fcd5b7b454fcc2ac25438e6660bc3cfd08
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-01-28 15:53:16 +0200

    fix layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,21 +7,29 @@ type Props = {
 };
 
 const Layout = ({ children }: Props) => {
-  // <source media="(min-width: 1000px)" srcSet="***url of image***">
-  {
-    /* <source media="(max-width: 999px)" srcSet="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==" sizes="100%"></source> */
-  }
   return (
     <>
-      <Image
-        alt=''
-        width='485'
-        height='280'
-        src={require('../images/game/counter-strike-16.jpg')}
-        webp={require('../images/game/counter-strike-16.jpg?webp')}
-        // className='absolute inset-x-0 top-0 z-[-1] w-full'
-        imgClassName='absolute inset-x-0 top-0 z-[-1] w-full hidden md:block'
-      />
+      <picture>
+        <source
+          media='(max-width: 999px)'
+          srcSet='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg=='
+          sizes='100%'
+        />
+        <source
+          media='(min-width: 1000px)'
+          srcSet={require('../images/game/counter-strike-16.jpg')}
+        />
+        <source
+          media='(min-width: 1000px)'
+          srcSet={require('../images/game/counter-strike-16.jpg?webp')}
+          type='image/webp'
+        />
+        <img
+          src={require('../images/game/counter-strike-16.jpg')}
+          alt='Counter-Strike 1.6'
+          className='absolute inset-x-0 top-0 z-[-1] w-full hidden lg:block'
+        />
+      </picture>
       <NavBar />
       <div className='relative main'>
         <main className='min-h-screen container pt-6'>{children}</main>
