52ed0b5525de3240e17ffc6d8656427e2cd67867
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-03-22 15:45:14 +0200

    update dl buttons

--- components/DownloadButtons.tsx
+++ components/DownloadButtons.tsx
@@ -46,7 +46,7 @@ const Buttons = (props: ButtonsProps) => {
     >
       <a
         // href='https://fleshas.lt/cs-download/Counter-Strike1.6.exe'
-        href='http://dl.fleshas.lt/cs/cstrike.exe'
+        href='https://dlfleshas.b-cdn.net/cs/cstrike.exe'
         ref={directBtn}
         className='py-1 pl-4 pr-6 mr-1 rounded-full flex bg-red-900'
       >
@@ -68,7 +68,7 @@ const Buttons = (props: ButtonsProps) => {
       </a>
       <a
         // href='https://fleshas.lt/cs-download/Counter-Strike 1.6.exe.torrent'
-        href='http://dl.fleshas.lt/cs/cstrike.exe.torrent'
+        href='https://dlfleshas.b-cdn.net/cs/cstrike.exe.torrent'
         className='py-1 pl-4 pr-6 rounded-full flex bg-red-900'
         ref={torrentBtn}
       >
